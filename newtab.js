a6f1efb51320128e1a3b0a3921e986de74f214d7
Sam Lakerveld <darkwater124@gmail.com>
2017-10-05 16:29:04 +0200

    Initial commit

---
+++ newtab.js
@@ -0,0 +1,47 @@
+let groupTemplate    = document.getElementById("tmpl-group");
+let bookmarkTemplate = document.getElementById("tmpl-bookmark");
+
+class Template {
+    constructor(templateElement) {
+        this.element = templateElement.content.cloneNode(true);
+    }
+
+    getElement(name) {
+        return this.element.querySelector(`[name="${name}"]`);
+    }
+}
+
+class Group extends Template {
+    constructor(bookmarkFolder) {
+        super(groupTemplate);
+
+        this.getElement("title").innerText = bookmarkFolder.title;
+
+        let bookmarks = bookmarkFolder.children.map(child => new Bookmark(child));
+        let slot = this.getElement("bookmarks");
+        bookmarks.forEach(bookmark => slot.appendChild(bookmark.element));
+    }
+}
+
+class Bookmark extends Template {
+    constructor(bookmark) {
+        super(bookmarkTemplate);
+
+        let title = this.getElement("title");
+        title.firstElementChild.innerText = bookmark.title;
+        title.href = bookmark.url;
+
+        let img = document.createElement("img");
+        img.src = "chrome://favicon/" + bookmark.url;
+        img.classList.add("bookmark-icon");
+        title.insertAdjacentElement("afterbegin", img);
+    }
+}
+
+chrome.bookmarks.getTree(tree => {
+    let others = tree[0].children.find(folder => folder.title == "Other bookmarks");
+    let newtab = others.children.find(folder => folder.title == "New Tab");
+
+    let groups = newtab.children.map(folder => new Group(folder));
+    groups.forEach(group => document.body.appendChild(group.element));
+});
